2c49341e8409528189937a5c32c2395c2a7367a9
Victor <victor.rodriguez02@utrgv.edu>
2016-11-28 22:56:48 -0600

    Testing ConvertHtmlToPdf
    Testing ConvertHtmlToPdf

--- Victor/ConvertHtmlToPdf/src/PdfPackage/PdfPackage.java
+++ Victor/ConvertHtmlToPdf/src/PdfPackage/PdfPackage.java
@@ -5,6 +5,9 @@
  */
 package PdfPackage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author VIC31
@@ -16,6 +19,20 @@ public class PdfPackage {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String htmlFile1 = "1.html";
+        String pdfFile1 = "1.pdf";
+        String htmlFile2 = "2.html";
+        String pdfFile2 = "2.pdf";
+        
+        // Testing the HTML 2 PDF Conversion
+        ConvertHtmlToPdfClass html2pdfObject1 = new ConvertHtmlToPdfClass(htmlFile1, pdfFile1);
+        html2pdfObject1.convert2Pdf(html2pdfObject1.getHtmlFileName(), html2pdfObject1.getPdfFileName());
+        
+        MergePDFClass pdfcom = new MergePDFClass();
+
+        List<String> pdf = new ArrayList<>();
+        
+        //pdfcom.combinePDFFiles(pdf, "newPDF.pdf");
     }
     
 }
